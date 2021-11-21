d11724bf40887019923c40f7f17a1fcb51e3aa00
Eva Abedin <eva.abedin@gmail.com>
2021-11-21 13:31:01 -0500

    FinalBootCamp

---
+++ BankOfAmerica/src/test/java/testhome/PopUpTest.java
@@ -0,0 +1,20 @@
+package testhome;
+
+import home.PopUp;
+import org.testng.annotations.Test;
+import reporting.TestLogger;
+
+public class PopUpTest extends PopUp{
+    @Test
+    public void testPopUp(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        goToPopUp();
+        popUpClick(driver);
+    }
+    @Test
+    public void testPopUpWrite() {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        goToPopUp();
+        popUpWrite(driver);
+    }
+}
